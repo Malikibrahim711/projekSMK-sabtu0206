c0056250f8adb7ffd155d8e5559804e25bd1ae5a
Roy <maulana.malik.ibrahim19@mhs.ubharajaya.ac.id>
2022-06-27 18:39:11 +0700

    edit create tahun ajaran

--- database/migrations/2022_06_27_080947_create_tahun_ajaran.php
+++ database/migrations/2022_06_27_080947_create_tahun_ajaran.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('tahun_ajaran', function (Blueprint $table) {
             $table->id();
+            $table->string('tahun_ajaran');
+            $table->string('status')->default('aktif');
             $table->timestamps();
         });
     }
