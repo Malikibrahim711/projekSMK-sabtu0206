1d42142a5c5ab77f6f2cae8ec991938567bdb030
Roy <maulana.malik.ibrahim19@mhs.ubharajaya.ac.id>
2022-06-27 18:40:15 +0700

    edit database nilai siswa

--- database/migrations/2022_06_27_080855_create_nilai_siswa.php
+++ database/migrations/2022_06_27_080855_create_nilai_siswa.php
@@ -15,6 +15,20 @@ return new class extends Migration
     {
         Schema::create('nilai_siswa', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('siswa_id');
+            $table->unsignedBigInteger('mapel_id');
+            $table->unsignedBigInteger('kelas_id');
+            $table->unsignedBigInteger('guru_id');
+            $table->unsignedBigInteger('semester_id');
+            $table->unsignedBigInteger('tahun_ajaran_id');
+            $table->unsignedBigInteger('nilai_id');
+            $table->unsignedBigInteger('nilai_huruf_id');
+            $table->unsignedBigInteger('nilai_angka_id');
+            $table->unsignedBigInteger('nilai_predikat_id');
+            $table->unsignedBigInteger('nilai_keterangan_id');
+            $table->unsignedBigInteger('nilai_keterangan_huruf_id');
+            $table->unsignedBigInteger('nilai_keterangan_angka_id');
+            $table->unsignedBigInteger('nilai_keterangan_predikat_id');
             $table->timestamps();
         });
     }
